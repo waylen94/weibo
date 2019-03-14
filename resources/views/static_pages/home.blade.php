4d19a012a8c907c5bf679b7800db64476f53cbc8
Weilun Liu <763027562@qq.com>
2019-03-14 05:45:25 +0000

    initializing basic layout

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
 @section('content')
-	<h1>主页</h1>
+	<div class="jumbotron">
+		<h1>Hello Laravel</h1>>
+		<p class = "lead">
+			this is a naughty or nice testing project
+		</p>
+		<p>
+		everything begin here
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="#" role="button">Sign In Now</a>
+		</p>
+		
+	</div>
 @stop
