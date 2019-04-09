5d4566309b500f6e93cb5969b2c6454d829c3a1a
Weilun Liu <763027562@qq.com>
2019-04-09 01:32:59 +0000

    home page content list

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Status;
+use Auth;
 
 class StaticPageController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+        
+        return view('static_pages/home', compact('feed_items'));
     }
+//     public function home()
+//     {
+//         return view('static_pages/home');
+//     }
     
     public function help()
     {
